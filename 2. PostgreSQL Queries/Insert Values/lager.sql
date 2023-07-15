2726eaeb7de08ab1a25f104c3d422d2c0f97d1f7
Nikita Ostrovskii <97059086+throughTheGalaxy@users.noreply.github.com>
2023-07-15 19:52:34 +0200

    Add files via upload

---
+++ 2. PostgreSQL Queries/Insert Values/lager.sql
@@ -0,0 +1,5 @@
+insert into lager (lager_name, lager_id, standort) values ('Lager1', 1, 'Berlin');
+insert into lager (lager_name, lager_id, standort) values ('Lager2', 2, 'Hamburg');
+insert into lager (lager_name, lager_id, standort) values ('Lager3', 3, 'Hannover');
+insert into lager (lager_name, lager_id, standort) values ('Lager4', 4, 'Frankfurt');
+insert into lager (lager_name, lager_id, standort) values ('Lager5', 5, 'Kiel');
